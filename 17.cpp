18350ac22638c69e110aa5f491780cd7e355c433
jorden134677 <48340565+jorden134677@users.noreply.github.com>
2019-05-31 11:15:43 +0800

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>　
+
+#include <stdlib.h> 
+
+int main(int argc, char *argv[]) {
+
+	int a[5]={0};
+
+	int i=0, min;
+
+	printf("請連續輸入五個數值：\n");
+
+	for(i=0;i<5;i++)
+
+	{
+
+	   printf("%d：", i+1 );
+
+	   scanf("%d", &a[i]);
+
+	}
+
+	min=a[0];
+
+	for(i=0;i<5;i++)
+
+	{
+
+	  if (min>a[i])	
+
+	  {
+
+		  min=a[i];
+
+	  }
+
+	}
+
+	printf("最小值是：%d\n\n",min); 
+
+	system("PAUSE");	
+
+	return 0;
+
+}
